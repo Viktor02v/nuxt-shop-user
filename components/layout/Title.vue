4f7b8292642790fe69dbb6f21793d1954c8d5758
Viktor Petruk <damnnn261@gmail.com>
2024-12-03 08:43:18 +0200

    some fixations in Title.vue

--- components/layout/Title.vue
+++ components/layout/Title.vue
@@ -1,14 +1,17 @@
 <script setup lang="ts">
-
+import { useMenuStore } from '@/store/menu.store';
+const menuStore = useMenuStore();
 </script>
 
 <template>
-	<h1 class="text-start text-[20px] md:text-4xl">
+	<h1 :class="{
+		'md:text-[20px]': menuStore.isMenuOpen,
+		'md:text-[30px]': !menuStore.isMenuOpen
+	}" class="text-right text-[20px] md:text-4xl">
 		<slot />
+		<span class="">|</span>
 	</h1>
 </template>
 
 
-<style scoped>
-
-</style>
+<style scoped></style>
--- pages/index.vue
+++ pages/index.vue
@@ -1,10 +1,15 @@
 <script setup lang="ts">
 
+
+useSeoMeta({
+	title: 'Overview | Nuxt-Shop'
+})
 </script>
 <template>
 	<div class="md:p-10 w-full h-[100vh]">
-		<LayoutTitle>
-			| Overview 
+		<LayoutTitle class="flex transition-all duration-300 items-center justify-end gap-2">
+			Overview
+			<span class="">|</span>
 		</LayoutTitle>
 	</div>
 </template>
