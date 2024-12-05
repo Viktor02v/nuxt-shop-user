fb28f88bdf11a7baaa18ed6b501417143afc4e6f
Viktor Petruk <damnnn261@gmail.com>
2024-12-05 08:47:03 +0200

    create filter

--- components/layout/FilterBar.vue
+++ components/layout/FilterBar.vue
@@ -1,10 +1,18 @@
 <script setup lang="ts">
+import { useFilterStore } from '@/store/filterbar.store'
+const filterStore = useFilterStore();
 
+const toggleFilter = () => {
+	filterStore.toggleMenu();
+}
 </script>
 
 <template>
-	<aside class="fixed left-0 bottom-0 w-[300px] bg-gradient-to-r from-[#020817] to-[#0F172A] h-[calc(100vh-80px)]">
-		<div class="w-full relative">
+	<aside :class="{
+		'translate-x-0': filterStore.isFilterOpen,
+		'translate-x-[0px] md:translate-x-[-300px]': !filterStore.isFilterOpen
+	}" class="fixed left-0 bottom-0 transition-all duration-500  bg-gradient-to-r from-[#020817] to-[#0F172A] h-[calc(100vh-80px)]">
+		<div class="w-[315px] relative">
 			<div
 				class="fixed top-[80px] flex text-2xl justify-center gap-2 items-center border-b left-0 w-[300px] h-[80px]">
 				<div>
@@ -26,7 +34,7 @@
 
 					<div>
 						<p class="text-center text-[1.2rem] pb-2">By Prices</p>
-						<ul class="flex underline border-b pb-8 flex-col gap-1">
+						<ul class="flex cursor-pointer underline border-b pb-8 flex-col gap-1">
 							<li>Search Cheapes</li>
 							<li>Search More Expensive</li>
 						</ul>
@@ -34,7 +42,7 @@
 
 					<div class="">
 						<p class="text-center text-[1.2rem] pb-2">By Vendors</p>
-						<ul class="flex underline flex-col gap-1">
+						<ul class="flex cursor-pointer underline flex-col gap-1">
 							<li>Nike</li>
 							<li>Adidas</li>
 							<li>Puma</li>
@@ -42,6 +50,10 @@
 						</ul>
 					</div>
 				</div>
+			</div>
+
+			<div class="fixed transition-all hover:scale-125 top-[450px] left-[300px]">
+				<Icon @click="toggleFilter" name="mynaui:panel-right-close" size="30" class="cursor-pointer" />
 			</div>
 		</div>
 	</aside>
