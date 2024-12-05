4018a8bcdeb93d0d78b321364de30fe4d9fde257
Viktor Petruk <damnnn261@gmail.com>
2024-12-05 09:25:37 +0200

    manage page shrinking when filter or menu is open

--- layouts/default.vue
+++ layouts/default.vue
@@ -1,7 +1,8 @@
 <script setup lang="ts">
 import { useMenuStore } from '@/store/menu.store';
 import { useIsLoadingStore } from '@/store/loader.store';
-
+import { useFilterStore } from '@/store/filterbar.store'
+const filterStore = useFilterStore();
 const menuStore = useMenuStore();
 const loadingStore = useIsLoadingStore();
 </script>
@@ -13,8 +14,12 @@ const loadingStore = useIsLoadingStore();
 			<LayoutTopNav />
 			<LayoutSidebar />
 			<div :class="{
-				'md:w-[1140px]': menuStore.isMenuOpen,
-				'md:w-[1390px]': !menuStore.isMenuOpen
+				'md:w-[1080px]': filterStore.isFilterOpen && !menuStore.isMenuOpen, // Only filter is open
+				'md:w-[1140px]': menuStore.isMenuOpen && !filterStore.isFilterOpen, // Only menu is open
+				'md:w-[830px]': menuStore.isMenuOpen && filterStore.isFilterOpen,  // Both are open
+				'md:w-[1390px]': !menuStore.isMenuOpen && !filterStore.isFilterOpen // Neither is open
+			}" :style="{
+				marginLeft: filterStore.isFilterOpen ? '310px' : ''
 			}" class="transition-all w-full duration-500 mt-[80px]">
 				<slot />
 			</div>
--- pages/collection/index.vue
+++ pages/collection/index.vue
@@ -1,15 +1,26 @@
 <script setup lang="ts">
-
+import { useFilterStore } from '@/store/filterbar.store'
+const filterStore = useFilterStore();
 </script>
 
 <template>
-	<div class="md:p-10 relative w-full h-[100vh]">
+	<div class="md:p-10 relative overflow-hidden w-full h-[100vh]">
 		<LayoutTitle class="flex transition-all mb-10 duration-300 items-center justify-end gap-2">
 			Collection
 		</LayoutTitle>
 
-		<LayoutFilterBar/>
+		<LayoutFilterBar />
+
+		<section id="">
+			<div>
+				Something
+			</div>
+		</section>
 	</div>
 </template>
 
-<style scoped></style>
+<style scoped>
+.shrink-section {
+	transition: width 0.3s ease-in-out;
+}
+</style>
