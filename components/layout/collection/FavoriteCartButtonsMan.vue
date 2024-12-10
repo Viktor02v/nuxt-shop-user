4b944f21029239c3908ad3fd143f12e19febd70e
Viktor Petruk <damnnn261@gmail.com>
2024-12-10 19:27:10 +0200

    fixations into (FavoriteCartButtonsMan.vue) component

--- components/layout/collection/FavoriteCartButtonsMan.vue
+++ components/layout/collection/FavoriteCartButtonsMan.vue
@@ -1,43 +1,47 @@
+<script setup lang="ts">
+import { useToggleFavoriteMan } from '@/composables/useToggleFavoriteMan';
+import { useToggleCartMan } from '@/composables/useToggleCartMan';
+import { defineProps } from 'vue';
+
+const props = defineProps({
+	item: {
+		type: Object,
+		required: true,
+	},
+});
+
+const toggleFavorite = useToggleFavoriteMan();
+const toggleCart = useToggleCartMan();
+</script>
+
 <template>
 	<div class="mt-2 flex gap-2 justify-end">
-		<div class="">
-			<UiButton v-if="!props.item?.isFavorite" @click.prevent="toggleFavorite.mutate(item)"
+		<!-- Favorite Button -->
+		<div>
+			<UiButton v-if="!props.item?.isFavorite" @click.prevent="toggleFavorite.mutate(props.item)"
 				class="p-2 flex items-center text-white rounded-full bg-[#0d193c]">
 				<Icon name="mynaui:heart" size="25" class="hover:scale-110 transition-all duration-400" />
 			</UiButton>
-			<UiButton v-else @click.prevent="toggleFavorite.mutate(item)"
+			<UiButton v-else @click.prevent="toggleFavorite.mutate(props.item)"
 				class="p-2 flex items-center text-white rounded-full bg-[#0d193c]">
 				<Icon name="majesticons:heart" size="25" class="hover:scale-110 transition-all text-red-600 duration-400" />
 			</UiButton>
 		</div>
 
-
-		<div class="">
-			<UiButton v-if="!props.item?.isAdded" @click.prevent="toggleCart.mutate(item)"
+		<!-- Cart Button -->
+		<div>
+			<UiButton v-if="!props.item?.isAdded" @click.prevent="toggleCart.mutate(props.item)"
 				class="p-2 flex items-center text-white rounded-full bg-[#0d193c]">
 				<Icon name="gg:add" size="26" class="hover:scale-110 transition-all duration-400" />
 			</UiButton>
-			<UiButton v-else @click.prevent="toggleCart.mutate(item)"
-				class="p-2 text-white  transition-all duration-400 flex items-center rounded-full bg-[#0d193c]">
+			<UiButton v-else @click.prevent="toggleCart.mutate(props.item)"
+				class="p-2 text-white transition-all duration-400 flex items-center rounded-full bg-[#0d193c]">
 				<Icon name="icons8:checked" size="26" class="hover:scale-110 transition-all text-green-600 duration-400" />
 			</UiButton>
 		</div>
 	</div>
 </template>
 
-<script setup lang="ts">
-import { useToggleFavoriteMan } from '@/composables/useToggleFavoriteMan'
-import { useToggleCartMan } from '@/composables/useToggleCartMan'
-
-import { defineProps } from 'vue';
-const props = defineProps({
-	item: Object,
-	default: {}
-})
-
-const toggleFavorite = useToggleFavoriteMan();
-const toggleCart = useToggleCartMan();
-</script>
-
 <style scoped>
+
 </style>
