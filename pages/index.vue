f1037f480c5d76107e36511a8b96e315352e6a07
Viktor Petruk <damnnn261@gmail.com>
2024-12-25 17:49:40 +0200

    final fixations in (home.vue, InfoBlocks.vue)

--- pages/index.vue
+++ pages/index.vue
@@ -18,12 +18,12 @@ const { activeStep, handleStepChange } = useStepNavigation();
 		<LayoutStepper />
 
 		<section id="step-1" :class="{ 'active': activeStep === 1, 'scale-100': activeStep !== 1 }"
-			class="w-full mb-[80px] transition-all duration-500 ease-in-out md:scroll-mt-[100px]">
+			class="w-full mb-[80px] transition-all duration-500 ease-in-out pb-5 md:pb-0 md:scroll-mt-[100px]">
 			<h1 class="font-bold text-center mb-5 md:mb-10 pt-5 md:pt-0 text-2xl md:text-4xl">Go to Collections</h1>
 			<LayoutLandingPageInfoBlock imgSrc="/overview/to-collection.jpeg">
 				<template #content>
 					<p
-						class="self-start text-sm md:text-xl text-white font-light leading-relaxed bg-gradient-to-r from-[#020817] to-[#0F172A] p-1 md:p-6 rounded-lg shadow-md hover:shadow-lg hover:md:scale-105 transition-transform duration-300 ease-in-out flex items-center">
+						class="self-start text-sm md:text-xl text-white font-light leading-relaxed bg-gradient-to-r from-[#020817] to-[#0F172A] p-1 md:p-6 md:rounded-lg shadow-md hover:shadow-lg hover:md:scale-105 transition-transform duration-300 ease-in-out flex items-center">
 						Discover the pathway to our exclusive collections, where style meets individuality. Whether you're
 						looking for timeless classics or contemporary designs, this section offers an immersive experience to
 						explore unique selections crafted to match your taste. Navigate seamlessly to uncover items curated
@@ -33,7 +33,7 @@ const { activeStep, handleStepChange } = useStepNavigation();
 				</template>
 				<template #button>
 					<UiButton
-						class="h-full text-white text-2xl bg-gradient-to-r from-[#020817] to-[#0F172A] hover:scale-105 transition-transform duration-300 ease-in-out">
+						class="h-full text-white text-2xl bg-gradient-to-r from-[#020817] to-[#0F172A] hover:md:scale-105 rounded-none md:rounded-lg transition-transform duration-300 ease-in-out">
 						<NuxtLink to="/collection" class="flex items-center gap-2">
 							<p>View Collections</p>
 							<Icon name="line-md:arrow-right" size="25" />
@@ -45,12 +45,12 @@ const { activeStep, handleStepChange } = useStepNavigation();
 
 		<section id="step-2"
 			:class="{ 'active': activeStep === 2, 'scale-100': activeStep !== 2 }"
-			class="w-full mb-[80px] transition-all duration-500  scroll-mt-[150px]">
+			class="w-full mb-[80px] transition-all duration-500 pb-5 md:pb-0  md:scroll-mt-[150px]">
 			<h1 class="font-bold text-center mb-5 md:mb-10 pt-5 md:pt-0 text-2xl md:text-4xl">Check Your Cart</h1>
 			<LayoutLandingPageInfoBlock2 imgSrc="/overview/to-cart.png">
 				<template #content>
 					<p
-						class="self-start text-sm md:text-xl text-white font-light leading-relaxed bg-gradient-to-r from-[#020817] to-[#0F172A] p-1 md:p-6 rounded-lg shadow-md hover:md:shadow-lg hover:md:scale-105 transition-transform duration-300 ease-in-out flex items-center">
+						class="self-start text-sm md:text-xl text-white font-light leading-relaxed bg-gradient-to-r from-[#020817] to-[#0F172A] p-1 md:p-6 md:rounded-lg shadow-md hover:md:shadow-lg hover:md:scale-105 transition-transform duration-300 ease-in-out flex items-center">
 						You've discovered the perfect pair of shoes to complement your unique style, and now it's time to make
 						them yours.
 						Transition seamlessly to your cart, where convenience meets clarity. Review your carefully selected
@@ -66,7 +66,7 @@ const { activeStep, handleStepChange } = useStepNavigation();
 				</template>
 				<template #button>
 					<UiButton
-						class="text-white text-2xl bg-gradient-to-r from-[#020817] to-[#0F172A] hover:scale-105 transition-transform duration-300 ease-in-out">
+						class="text-white text-2xl bg-gradient-to-r from-[#020817] to-[#0F172A] hover:md:scale-105 rounded-none md:rounded-lg transition-transform duration-300 ease-in-out">
 						<NuxtLink to="/cart" class="flex items-center gap-2">
 							<p>View Cart</p>
 							<Icon name="line-md:arrow-right" size="25" />
@@ -78,12 +78,12 @@ const { activeStep, handleStepChange } = useStepNavigation();
 
 		<section id="step-3"
 			:class="{ 'active': activeStep === 3, 'scale-100': activeStep !== 3 }"
-			class="w-full transition-all duration-500">
+			class="w-full transition-all pb-5 md:pb-0 duration-500">
 			<h1 class="font-bold text-center mb-5 md:mb-10 pt-5 md:pt-0 text-2xl md:text-4xl">Make Order</h1>
 			<LayoutLandingPageInfoBlock3 imgSrc="/overview/to-orders.jpeg">
 				<template #content>
 					<p
-						class="self-start text-sm md:text-xl text-white font-light leading-relaxed bg-gradient-to-r from-[#020817] to-[#0F172A] p-1 md:p-6 rounded-lg shadow-md hover:md:shadow-lg hover:md:scale-105 transition-transform duration-300 ease-in-out flex items-center">
+						class="self-start text-sm md:text-xl text-white font-light leading-relaxed bg-gradient-to-r from-[#020817] to-[#0F172A] p-1 md:p-6 md:rounded-lg shadow-md hover:md:shadow-lg hover:md:scale-105 transition-transform duration-300 ease-in-out flex items-center">
 						Your selections are ready to take the next step toward becoming a part of your life. Transition
 						seamlessly to the orders section, where every detail of your purchase is carefully tracked for your
 						peace of mind. From processing to shipment, stay informed about the status of your items with
@@ -95,7 +95,7 @@ const { activeStep, handleStepChange } = useStepNavigation();
 				</template>
 				<template #button>
 					<UiButton
-						class="h-full text-white text-2xl bg-gradient-to-r from-[#020817] to-[#0F172A] hover:scale-105 transition-transform duration-300 ease-in-out">
+						class="h-full text-white text-2xl bg-gradient-to-r from-[#020817] to-[#0F172A] hover:md:scale-105 rounded-none md:rounded-lg transition-transform duration-300 ease-in-out">
 						<NuxtLink to="/orders" class="flex items-center gap-2">
 							<p>Make Order</p>
 							<Icon name="line-md:arrow-right" size="25" />
