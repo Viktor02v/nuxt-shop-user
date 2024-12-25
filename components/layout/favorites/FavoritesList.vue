f3d4b82bea7e225c61f451332ea69ebe1b0b3193
Viktor Petruk <damnnn261@gmail.com>
2024-12-25 18:09:42 +0200

    attempt to debug (Favorites.vue) #2

--- components/layout/favorites/FavoritesList.vue
+++ components/layout/favorites/FavoritesList.vue
@@ -36,13 +36,13 @@ const toggleCart = (item: any) => {
 			</template>
 		</LayoutMessageBlock>
 
-		<div v-if="favoriteItems" class="p-5 sm:pr-0 md:pr-0 lg:pr-0 xl:pr-0 grid grid-cols-1 sm:grid-cols-2 md:grid-cols-3 lg:grid-cols-4 xl:grid-cols-5 gap-4">
+		<div v-if="favoriteItems" class="p-5 pr-[50px] sm:pr-0 md:pr-0 lg:pr-0 xl:pr-0 grid grid-cols-1 sm:grid-cols-2 md:grid-cols-3 lg:grid-cols-4 xl:grid-cols-5 gap-4">
 			<div v-for="item in favoriteItems" :key="item.$id" class="animation">
 				<NuxtLink :to="item.isMan ? `/collection/itemMan/${item.$id}` : `/collection/itemWoman/${item.$id}`"
 					class="border animation hover:md:scale-105 transition-all duration-500 rounded py-5 px-2 flex flex-col items-center bg-gradient-to-b from-[#020817] to-[#0F172A]">
 					<div class="flex flex-col">
 						<div class="min-h-[220px] max-h-[220px] overflow-hidden p-5 md:p-0 lg:p-0  xl:p-0 flex flex-col  justify-center">
-							<NuxtImg :src="item.foto_url" width="400" class="mb-4  rounded" />
+							<NuxtImg :src="item.foto_url" width="400" class="mb-4 rounded" />
 						</div>
 
 						<div class="flex h-[190px] overflow-y-auto gap-1 mb-2 flex-col">
