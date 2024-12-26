985871a042dcd2f3e0bfd69359ff9ba3b7b3c626
Viktor Petruk <damnnn261@gmail.com>
2024-12-26 10:29:55 +0200

    create (MobileBar.vue) component

---
+++ components/layout/sidebar/MobileBar.vue
@@ -0,0 +1,29 @@
+<script setup lang="ts">
+import { useAdaptiveStore } from '@/store/adaptive.store';
+import { MENU_DATA } from '~/components/layout/sidebar/menu.data'
+
+const adaptiveStore = useAdaptiveStore();
+</script>
+<template>
+<div v-if="adaptiveStore.isMobile" class="fixed w-[390px] top-[80px] bg-gradient-to-b from-[#020817] to-[#0F172A] px-5  z-10">
+	<div class="flex items-center justify-between py-3">
+		<NuxtLink :to="item.url" v-for="item in MENU_DATA " :key="item.name" @click="toggleMenu"
+			class="text-[25px] flex items-center text-white hover:text-red-500 transition easy-in-out ">
+			<Icon :name="item.icon" />
+			<span v-if="!adaptiveStore.isMobile">{{ item.name }}</span>
+		</NuxtLink>
+
+		<a href="https://nuxt-shop-crm02v.netlify.app" target="_blank"
+			class="text-[25px] flex items-center text-white hover:text-red-500 transition easy-in-out ">
+			<Icon name="dashicons:admin-network" />
+			<span v-if="!adaptiveStore.isMobile">Admin</span>
+		</a>
+	</div>
+</div>
+</template>
+
+
+
+<style scoped>
+
+</style>
--- layouts/default.vue
+++ layouts/default.vue
@@ -1,18 +1,17 @@
 <script setup lang="ts">
 import { useMenuStore } from '@/store/menu.store';
-import { useIsLoadingStore } from '@/store/loader.store';
 import { useFilterStore } from '@/store/filterbar.store'
 const filterStore = useFilterStore();
 const menuStore = useMenuStore();
-const loadingStore = useIsLoadingStore();
 </script>
 
 <template>
 	<div>
 		<!-- <LayoutLoader v-if="loadingStore.isLoading" /> -->
-		<section class=" relative w-[380px] h-[100vh]">
+		<section class=" relative w-[390px] md:w-[380px] h-[100vh]">
 			<LayoutTopNav />
 			<LayoutSidebar />
+			<LayoutSidebarMobileBar/>
 			<div :class="{
 				'md:w-[1080px]': filterStore.isFilterOpen && !menuStore.isMenuOpen, // Only filter is open
 				'md:w-[1140px]': menuStore.isMenuOpen && !filterStore.isFilterOpen, // Only menu is open
@@ -20,7 +19,7 @@ const loadingStore = useIsLoadingStore();
 				'md:w-[1390px]': !menuStore.isMenuOpen && !filterStore.isFilterOpen // Neither is open
 			}" :style="{
 				marginLeft: filterStore.isFilterOpen ? '310px' : ''
-			}" class="transition-all w-full h-full duration-500 mt-[80px]">
+			}" class="transition-all w-full h-full duration-500 mt-[120px] md:mt-[80px]">
 				<slot />
 			</div>
 		</section>
@@ -28,6 +27,4 @@ const loadingStore = useIsLoadingStore();
 </template>
 
 
-<style scoped>
-
-</style>
+<style scoped></style>
